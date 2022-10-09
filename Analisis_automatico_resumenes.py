c4a27a06aa3d42829009f46055f436ad4fb45048
Diego Díaz <diazfonseca.diego@gmail.com>
2022-10-09 17:41:46 -0600

    Actualización del generador del nuevo dataset

--- Analisis_automatico_resumenes.py
+++ Analisis_automatico_resumenes.py
@@ -1,24 +1,80 @@
-from pyrfc3339 import generate
+import textwrap
+import nltk
+import operator
 import sumy.summarizers.text_rank as TextRankSummarizer
 import sumy.summarizers.lsa       as LsaSummarizer
 import sumy.parsers.plaintext     as PlaintTextParser
 import sumy.nlp.tokenizers        as Tokenizer
 import pandas                     as pd
 import numpy                      as np
-import textwrap
-import nltk
-import operator
+from tqdm        import tqdm
 from rouge_score import rouge_scorer
 from functools   import reduce
-
+from sys         import argv
 nltk.download('punkt')
 
-AMOUNT_LINES_PER_SUMMARY = 5
-AMOUNT_SUMMARIES = 30
+# Constants
 SUMMARIZERS = [
     TextRankSummarizer.TextRankSummarizer(),
     LsaSummarizer.LsaSummarizer()
 ]
+FIRST_COLUMNS_FOR_GENERATED_DF = ["Texto original", "Resumen humano"]
+SEED_FOR_SUMMARY_SELECTION = 616
+AMOUNT_LINES_PER_SUMMARY   = 5
+AMOUNT_SUMMARIES           = 30
+ROUGE_METRICS              = ['rouge2', 'rougeL']
+COLUMNS_FROM_INPUT_DF_ENGLISH = ["article", "highlights"]
+COLUMNS_FROM_INPUT_DF_SPANISH = ["Fuente", "Resumen"]
+
+# Summary generation
+def main():
+    language = argv[2]
+    columnsFromInputDF   = getColumnsFromInputDF(language)
+    generated_summaries  = createDataFrame()
+    docs, humanSummaries = getNewsAndHumanSummaries(columnsFromInputDF)
+    scorer               = rouge_scorer.RougeScorer(ROUGE_METRICS)
+    fillDataFrame(generated_summaries, docs, humanSummaries, scorer, language)
+
+    generated_summaries.to_csv(argv[3])
+
+def getColumnsFromInputDF(language):
+    if language == "spanish": return COLUMNS_FROM_INPUT_DF_SPANISH
+    else: return COLUMNS_FROM_INPUT_DF_ENGLISH
+
+def createDataFrame():
+    columns = FIRST_COLUMNS_FOR_GENERATED_DF
+    for summarizer in SUMMARIZERS:
+        summarizer_name = type(summarizer).__name__
+        columns.append(summarizer_name)
+        for metric in ROUGE_METRICS:
+            columns.append(type(summarizer).__name__ + " " + metric + " precision")
+            columns.append(type(summarizer).__name__ + " " + metric + " exhautividad")
+    return pd.DataFrame(index=range(AMOUNT_SUMMARIES), columns=columns)
+
+def getNewsAndHumanSummaries(columnsFromInputDF):
+    np.random.seed(SEED_FOR_SUMMARY_SELECTION)
+    rng = np.random.default_rng()
+    dataFrame     = pd.read_csv(argv[1])
+    amount_news   = dataFrame.shape[0]
+    selected_news = rng.integers(
+        amount_news, size = (AMOUNT_SUMMARIES,)
+    )
+    sample = dataFrame.iloc[selected_news]
+    return sample[columnsFromInputDF[0]], sample[columnsFromInputDF[1]]
+
+def fillDataFrame(generated_summaries, docs, humanSummaries, scorer, language):
+    for i, doc in enumerate(tqdm(docs, desc="Documentos")):
+        hummanSummary = humanSummaries.iloc[i]
+        new_data_row  = [doc, hummanSummary]
+        for summarizer in SUMMARIZERS:
+            generated_summary = makeSummary(doc, summarizer, language)
+            scores            = scorer.score(hummanSummary, generated_summary)
+            new_data_row.append(generated_summary)
+            for metric in ROUGE_METRICS:
+                new_data_row.append(scores[metric].precision)
+                new_data_row.append(scores[metric].recall)
+        
+        generated_summaries.iloc[i] = new_data_row
 
 def makeSummary(doc2summarize, summarizer, language):
     parser = PlaintTextParser.PlaintextParser.from_string(
@@ -26,45 +82,7 @@ def makeSummary(doc2summarize, summarizer, language):
         Tokenizer.Tokenizer(language)
     )
     summaryOBJ = summarizer(parser.document, sentences_count=AMOUNT_LINES_PER_SUMMARY)
-    return reduce(operator.add, map(lambda x: str(x)+"\n", summaryOBJ), "")
-
-def main():
-    dataFrame = pd.read_csv("./datos/test.csv")
-    language  = "english"
-
-    rng = np.random.default_rng()
-    amount_news   = dataFrame.shape[0]
-    selected_news = rng.integers(
-        amount_news, size = (AMOUNT_SUMMARIES,)
-    )
-    entry = dataFrame.iloc[selected_news]
-    docs  = entry["article"]
-    humanSummaries = entry["highlights"]
-
-    columns = ["Original Text", "Human summary"]
-    for summarizer in SUMMARIZERS:
-        summarizer_name = type(summarizer).__name__
-        columns.append(summarizer_name)
-        columns.append(type(summarizer).__name__ + "Rouge precition")
-        columns.append(type(summarizer).__name__ + "Rouge recall")
-
-    generated_summaries = pd.DataFrame(index=range(30), columns=columns)
-    scorer = rouge_scorer.RougeScorer(['rouge1', 'rougeL'])
-
-    for i, doc in enumerate(docs):
-        hummanSummary = humanSummaries.iloc[i]
-        new_data_row = [doc, hummanSummary]
-        for summarizer in SUMMARIZERS:
-            generated_summary = makeSummary(doc, summarizer, language)
-            scores = scorer.score(hummanSummary, generated_summary)
-            new_data_row.append(generated_summary)
-            new_data_row.append(scores[0])
-            new_data_row.append(scores[1])
-        
-        generated_summaries.iloc[i] = new_data_row
-
-    
-    print(generated_summaries)
+    return reduce(operator.add, map(lambda x: str(x), summaryOBJ), "")
 
 if __name__=="__main__":
     main()
