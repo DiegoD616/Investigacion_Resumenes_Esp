a01425c048e84ee4b2b492695b67e31f01b861d2
Diego Díaz <diazfonseca.diego@gmail.com>
2022-11-09 18:29:35 -0600

    Generación de gráficos para el analisis automático

--- Analisis_resultados.R
+++ Analisis_resultados.R
@@ -1,4 +1,5 @@
 library("dplyr")
+library("ggplot2")
 
 NOMBRE_ARCHIVO_espanol = "./datos_salida/Resumenes_generados_espanol.csv"
 NOMBRE_ARCHIVO_ingles  = "./datos_salida/Resumenes_generados_ingles.csv"
@@ -49,39 +50,93 @@ for( datos in list(datos_espanol, datos_ingles)) {
 
 summary(Rendimiento_resumidores %>% select_if(is.numeric))
 
+coded = Rendimiento_resumidores %>% 
+  mutate(Idioma = if_else(Idioma == "Español","Esp","Eng")) %>%
+  mutate(Resumidor = if_else(Resumidor == "Lsa","L","T"))
+
 res.man <- manova(
   cbind(rouge2.precision, rouge2.exhaustividad, 
         rougeL.precision, rougeL.exhaustividad) ~ Idioma * Resumidor, 
   data = Rendimiento_resumidores)
 summary(res.man)
 
-par(mfrow=c(2,2))
 two.way <- aov(rouge2.precision ~ Idioma * Resumidor,     data = Rendimiento_resumidores)
 summary(two.way)
-plot(two.way)
 
 two.way <- aov(rougeL.precision ~ Idioma * Resumidor,     data = Rendimiento_resumidores)
 summary(two.way)
-plot(two.way)
 
-two.way <- aov(rouge2.exhaustividad ~ Idioma * Resumidor, data = Rendimiento_resumidores)
-summary(two.way)
-
-
-two.way <- aov(rouge2.exhaustividad ~ Idioma * Resumidor, data = Rendimiento_resumidores)
+###### Diferencias en la exhausistividad R2
+two.way <- aov(rouge2.exhaustividad ~ Idioma + Resumidor, data = Rendimiento_resumidores)
+par(mfrow=c(2,2))
 summary(two.way)
 plot(two.way)
+
+
+two.way <- aov(rouge2.exhaustividad ~ Idioma:Resumidor, data = coded)
 par(mfrow=c(1,1))
-tukey.two.way<-TukeyHSD(two.way)
-plot(tukey.two.way)
+tukey.two.way <- TukeyHSD(two.way)
+plot(tukey.two.way, las=1, cex.axis=0.7)
+
+rouge2.exhaustividad.media <- Rendimiento_resumidores %>%
+  group_by(Idioma, Resumidor) %>%
+  summarise(
+    rouge2.exhaustividad = mean(rouge2.exhaustividad)
+  )
+
+R2.Exhaustividad.plot <- ggplot(
+    Rendimiento_resumidores, 
+    aes(x = Idioma, y = rouge2.exhaustividad, group=Resumidor)
+  ) +
+  geom_point(
+    cex = 1.5, pch = 1.0, 
+    position = position_jitter(w = 0.1, h = 0)
+  ) +
+  stat_summary(fun.data = 'mean_se', geom = 'errorbar', width = 0.2) +
+  stat_summary(fun.data = 'mean_se', geom = 'pointrange') +
+  geom_point(
+    data = rouge2.exhaustividad.media, 
+    aes(x = Idioma, y = rouge2.exhaustividad)
+  ) +
+  facet_wrap(~Resumidor) +
+  theme_linedraw() +
+  labs(title = "", x = "Idioma", y = "Exhaustividad de Rouge2")
+R2.Exhaustividad.plot
 
 
-two.way <- aov(rougeL.exhaustividad ~ Idioma * Resumidor, data = Rendimiento_resumidores)
-summary(two.way)
 
-
+###### Diferencias en la exhausistividad RL
 two.way <- aov(rougeL.exhaustividad ~ Idioma + Resumidor, data = Rendimiento_resumidores)
+par(mfrow=c(2,2))
 summary(two.way)
 plot(two.way)
+
+two.way <- aov(rougeL.exhaustividad ~ Idioma:Resumidor, data = coded)
 tukey.two.way<-TukeyHSD(two.way)
-tukey.two.way
+par(mfrow=c(1,1))
+plot(tukey.two.way, las=1, cex.axis=0.7)
+
+rougeL.exhaustividad.media <- Rendimiento_resumidores %>%
+  group_by(Idioma, Resumidor) %>%
+  summarise(
+    rougeL.exhaustividad = mean(rougeL.exhaustividad)
+  )
+
+RL.Exhaustividad.plot <- ggplot(
+    Rendimiento_resumidores, 
+    aes(x = Idioma, y = rougeL.exhaustividad, group=Resumidor)
+  ) +
+  geom_point(
+    cex = 1.5, pch = 1.0, 
+    position = position_jitter(w = 0.1, h = 0)
+  ) +
+  stat_summary(fun.data = 'mean_se', geom = 'errorbar', width = 0.2) +
+  stat_summary(fun.data = 'mean_se', geom = 'pointrange') +
+  geom_point(
+    data = rougeL.exhaustividad.media, 
+    aes(x = Idioma, y = rougeL.exhaustividad)
+  ) +
+  facet_wrap(~Resumidor) +
+  theme_linedraw() +
+  labs(title = "", x = "Idioma", y = "Exhaustividad de RougeL")
+RL.Exhaustividad.plot
